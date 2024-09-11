4b5dec71def65f7f50ab8d903353d153574f6cbf
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-09-11 23:29:33 +0530

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "you hit the singup",
+    });
+}
+
+export const login = async (req, res) => {
+    res.json({
+        data: "you hit the li",
+    });
+} 
+
+export const logout = async (req, res) => {
+    res.json({
+        data: "you hit the lo",
+    });
+} 
