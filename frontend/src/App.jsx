25974eed2070ccab7d558974726e94f42f60fe6a
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-10-30 14:27:04 +0530

    added ui frontend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+
+import LoginPage from "./pages/auth/login/LoginPage"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+import HomePage from "./pages/home/HomePage"
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+
+
+
 function App() {
 
   return (
-    <>
-      <p className='text-5xl text-red-500 bg-black '>hiiiiii</p>
-    </>
+      <div className='flex max-w-6xl mx-auto' >
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={ <HomePage/> } />
+          <Route path='/login' element={ <LoginPage/> } />
+          <Route path='/signup' element={ <SignUpPage /> } />
+          <Route path='/notifications' element={ <NotificationPage /> } />
+          <Route path='/profile/:username' element={ <ProfilePage /> } />
+        </Routes>
+        <RightPanel/>
+      </div>
   )
 }
 
 export default App
 
-// 2:50:00
+// 3:25:00
